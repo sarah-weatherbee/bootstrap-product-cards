0d014d0e4d5c1a2ab724c588fd37652fcb6590a3
Sarah Weatherbee <sarahmweatherbee@gmail.com>
2019-03-19 21:05:13 -0500

    used grid system

--- main.js
+++ main.js
@@ -34,7 +34,7 @@ const products = [
         validUntil: '04/01/2016'
     },
     {
-        title: 'product3',
+        title: 'product4',
         description: 'bla bla bla',
         imageUrl: 'https://media.npr.org/assets/img/2017/04/25/istock-115796521-fcf434f36d3d0865301cdcb9c996cfd80578ca99-s800-c85.jpg',
         size: '4" x 4"',
@@ -45,7 +45,7 @@ const products = [
         validUntil: '04/01/2016'
     },
     {
-        title: 'product3',
+        title: 'product5',
         description: 'bla bla bla',
         imageUrl: 'https://media.npr.org/assets/img/2017/04/25/istock-115796521-fcf434f36d3d0865301cdcb9c996cfd80578ca99-s800-c85.jpg',
         size: '4" x 4"',
@@ -63,19 +63,13 @@ const printToDom = (divId, textToPrint) => {
 };
 const cardBuilder = (merp) => {
     let domString = '';
+    domString += `<div class="container">`;
+    domString += `<div class="row">`;
     merp.forEach((product) => {
-        domString += '<div class="card">';
-        domString += `<h2>${product.title}</h2>`;
-        domString += `<h3>${product.description}</h3>`;
-        domString += `<img src="${product.imageUrl}" class="card-image">`;
-        domString += `<h3>${product.size}</h3>`;
-        domString += `<h3>${product.weight}</h3>`;
-        domString += `<h3>${product.price1}</h3>`;
-        domString += `<h3>${product.price2}</h3>`;
-        domString += `<h3>${product.price3}</h3>`;
-        domString += `<h3>${product.validUntil}</h3>`;
-        domString += '</div>';
+        domString += `<div class="col-4">${product.title}</div>`;
     })
+    domString += `</div>`;
+    domString += `</div>`;
     printToDom('some-cards', domString);
 };
 
